57d5bcd709fd7e0f8f7f7ab0ed7919470edf222d
João Marcos <joaomarcos.vidcar@gmail.com>
2025-07-16 14:20:15 -0300

    feat(use-case): add register and unit test

---
+++ src/use-cases/register/register.spec.ts
@@ -0,0 +1,54 @@
+import { it, describe, expect, beforeEach } from 'vitest'
+import { InMemoryUsersRepository } from '@/repositories/in-memory-repository/in-memory-users-repository'
+import { RegisterUseCase } from '@/use-cases/register/register'
+import { UsersRepository } from '@/repositories/users-repository'
+import { UserAlreadyExistsError } from '../errors/user-already-exists-error'
+import { compare } from 'bcryptjs'
+
+describe('Register Use Case', () => {
+	let usersRepository: UsersRepository
+	let sut: RegisterUseCase
+
+	beforeEach(() => {
+		usersRepository = new InMemoryUsersRepository()
+		sut = new RegisterUseCase(usersRepository)
+	})
+
+	const [name, email, password] = ['John Doe', 'johndoe@example.com', '123456']
+
+	it('should be possible to register', async () => {
+		const { user } = await sut.use({
+			name,
+			email,
+			password
+		})
+
+		expect(user.id).toEqual(expect.any(String))
+	})
+
+	it('should not be possible to register with a duplicated email.', async () => {
+		await sut.use({
+			name,
+			email,
+			password
+		})
+
+		await expect(async () => {
+			await sut.use({
+				name,
+				email,
+				password
+			})
+		}).rejects.toBeInstanceOf(UserAlreadyExistsError)
+	})
+
+	it('should have its user password hashed upon registration', async () => {
+		const { user } = await sut.use({
+			name,
+			email,
+			password
+		})
+
+		expect(await compare(password, user.password_hash)).toEqual(true)
+	})
+})
