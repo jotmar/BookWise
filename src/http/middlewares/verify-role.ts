f67d7f09eb7988aa8d84824b4049f75dd08a737a
João Marcos <joaomarcos.vidcar@gmail.com>
2025-07-26 14:09:49 -0300

    feat(middleware): change response structure for consistency

--- src/http/middlewares/verify-role.ts
+++ src/http/middlewares/verify-role.ts
@@ -3,7 +3,7 @@ import { FastifyReply, FastifyRequest } from 'fastify'
 export function verifyRole(roleToVerify: 'MEMBER' | 'ADMIN') {
 	return async (request: FastifyRequest, reply: FastifyReply) => {
 		if (request.user.role !== roleToVerify) {
-			return reply.status(401).send('Unauthorized')
+			return reply.status(401).send({ message: 'Unauthorized' })
 		}
 	}
 }
